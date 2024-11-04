49651bb5458b9b3be5ec207b1bec9ce92ccc518e
Luca Fischer <luca.fischer@btc-ag.com>
2024-11-04 15:24:03 +0100

    Enum für Colors erstellt

---
+++ src/main/java/com/btcag/bootcamp/Classes/Colors.java
@@ -0,0 +1,21 @@
+package com.btcag.bootcamp.Classes;
+
+public enum Colors {
+    RED("\u001b[31;1m1"),
+    BLUE("\u001B[34m"),
+    GREEN("\u001B[32m"),
+    YELLOW("\u001B[33m"),
+    MAGENTA("\u001B[35m"),
+    PURPLE("\u001B[35m"),
+    STOP("\u001B[0m");
+
+    private final String value;
+
+    Colors(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
