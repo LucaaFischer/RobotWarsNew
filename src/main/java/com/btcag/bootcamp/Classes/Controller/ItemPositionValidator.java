9ef9527d2b1cb1be990a35b229f8a51b0c4ff1c7
Luca Fischer <luca.fischer@btc-ag.com>
2024-12-06 08:08:55 +0100

    Items can't spawn on Wallfields anymore

--- src/main/java/com/btcag/bootcamp/Classes/Controller/ItemPositionValidator.java
+++ src/main/java/com/btcag/bootcamp/Classes/Controller/ItemPositionValidator.java
@@ -1,11 +1,12 @@
 package com.btcag.bootcamp.Classes.Controller;
 
 import com.btcag.bootcamp.Classes.Models.Items;
+import com.btcag.bootcamp.Classes.Models.MapWallField;
 
 public class ItemPositionValidator {
     public static boolean validator(Items[] items, int x, int y) {
         for(Items item : items) {
-            if(item.getItemX() == x && item.getItemY() == y) {
+            if(item.getItemX() == x && item.getItemY() == y || MapWallField.wallField(x, y)) {
                 return true;
             }
         }
